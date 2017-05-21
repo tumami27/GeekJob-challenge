0168a1f587e3bcdd4c8c53a50c4ed02f81f29777
matumotoyuuya <tumami27kan@gmail.com>
2017-05-21 16:14:22 +0900

    引数、戻り値１

---
+++ CampChallenge_java/CampChallenge_java/003.JSPとサーブレット１/modoriti3.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+@WebServlet(name = "modoriti3", urlPatterns = {"/modoriti3"})
+public class modoriti3 extends HttpServlet {
+String name[] = {"松本", "名前2", "名前3"};
+     String birthday[] = {"９月２３", "生年月日2", "生年月日3"};
+     String address[] = {"埼玉", "住所2", "住所3"};
+    
+     String person (int id){
+         String profile ="" ;
+         
+         if (id == 1){
+             profile =name[0] + ",　" + birthday[0] + ",　" + address[0];
+         }
+         if (id == 2){
+             profile = name[1] + ",　" + birthday[1] + ",　" + address[1];
+         }
+         if (id == 3){
+             profile = name[2] + ",　" + birthday[2] + ",　" + address[2];
+         }
+         
+         return profile;
+     }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet modoriti3</title>");            
+            out.println("</head>");
+            out.println("<body>");
+             out.print(person(1));
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
